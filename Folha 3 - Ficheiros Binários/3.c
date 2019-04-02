f21b0fa9e64de320b2bd78e3c9ea202b71556e3b
Diogo Lavareda <diogolavareda9@hotmail.com>
2019-04-02 13:47:59 +0100

    update

--- Folha 3 - Ficheiros Binários/3.c
+++ Folha 3 - Ficheiros Binários/3.c
@@ -35,27 +35,57 @@ int main()
     fclose(fb);
 
     //B ,C e D
-    float var;
-    int tm;
-    float media;
-    fb = fopen("/home/dlavareda/Documents/UBI/Programação 2/programacao2/Folha 3 - Ficheiros Binários/registos.bin", "rb");
-    fseek(fb, sizeof(int), SEEK_SET); //posiciona-se no 1º var
-    while (fread(&var, sizeof(float), 1, fb) == 1)
-    {                                    //le o var e posiciona-se no tm
-        fread(&tm, sizeof(int), 1, fb);  //le o TM e posiciona-se no proximo ref
-        fseek(fb, sizeof(int), SEEK_CUR); // avança até ao proximo var
-        media = (tm+var)/2;
-        printf("Media = %f\n", media);
-    }
-    fclose(fb);
-}
 
-/*
-b) determine a média dos valores de cada uma das variáveis da experiência;
-c) determine a medição com o melhor (menor) coeficiente de aproximação, sendo este
-dado pela expressão:
-1 + ((var - mvar) / mvar) + ((tm - mtm) / mtm)
-em que mvar e mtm representam as médias calculadas anteriormente e que se
-assume serem diferentes de zero;
-d) mostre as médias calculadas e os dados da medição com o melhor coeficiente de
-aproximação.*/
+    fb = fopen("/home/dlavareda/Documents/UBI/Programação 2/programacao2/Folha 3 - Ficheiros Binários/registos.bin", "rb");
+    float varaux;
+    int tmaux, refaux;
+    float mediavar, mediat, *var;
+    int *tm, *ref;
+    var = malloc(sizeof(float));
+    tm = malloc(sizeof(int));
+    ref = malloc(sizeof(int));
+    int i = 1;
+
+    mediavar = 0;
+    mediat = 0;
+    while (fread(&refaux, sizeof(int), 1, fb) == 1)
+    {
+        fread(&varaux, sizeof(float), 1, fb); //le o var e posiciona-se no tm
+        fread(&tmaux, sizeof(int), 1, fb);    //le o TM e posiciona-se no proximo ref
+        ref = realloc(ref, (i) * sizeof(int));
+        ref[i - 1] = refaux;
+        var = realloc(var, (i) * sizeof(float));
+        var[i - 1] = varaux;
+        tm = realloc(tm, (i) * sizeof(float));
+        tm[i - 1] = tmaux;
+        mediavar = mediavar + varaux;
+        mediat = mediat + tmaux;
+        printf("REF = %d\n", ref[i - 1]);
+        printf("VAR = %f\n", var[i - 1]);
+        printf("TM = %d\n", tm[i - 1]);
+        i++;
+    }
+    mediavar = mediavar / i;
+    mediat = mediat / i;
+
+    fclose(fb);
+    float melhorcaprox = 0;
+    float caprox = 0;
+    int melhor_i=0;
+    for (int ii = 0; ii < i; ii++)
+    {
+        caprox = (1 + ((var[ii] - mediavar) / mediavar) + ((tm[ii] - mediat) / mediat));
+        if (caprox > melhorcaprox)
+        {
+            melhorcaprox = caprox;
+            melhor_i = ii;
+        }
+    }
+    printf("\n");
+    printf("Media Var = %f\n", mediavar);
+    printf("Media TM = %f\n", mediat);
+    printf("MELHOR CAPROX = %f\n", melhorcaprox);
+    printf("MEDIÇÃO COM O MELHOR CAPROX = %d\n", ref[melhor_i]);
+    printf("VAR MELHOR MEDICAO = %f\n", var[melhor_i]);
+    printf("TM MELHOR MEDIÇÃO = %d\n", tm[melhor_i]);
+}
