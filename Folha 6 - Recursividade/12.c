64cbece731c719e041e0d9ba5835a056a834675e
Diogo Lavareda <diogolavareda9@hotmail.com>
2019-04-02 22:54:55 +0100

    update

--- Folha 6 - Recursividade/12.c
+++ Folha 6 - Recursividade/12.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "recursividade.h"
 int numerosPares(int v[], int n)
 {
     if (n == 0)
@@ -6,10 +8,19 @@ int numerosPares(int v[], int n)
     }
     if ((v[n - 1]) % 2 == 0)
     {
-        return 1 + numerosPares(x, n - 1);
+        return 1 + numerosPares(v, n - 1);
     }
     else
     {
-        return numerosPares(x, n - 1);
+        return numerosPares(v, n - 1);
     }
+}
+int main()
+{
+    int pares;
+    int v;
+    int tam;
+    v = LerVectorFloatFicheiro("/home/dlavareda/Documents/UBI/Programação 2/programacao2/Folha 6 - Recursividade/Inteiros.txt", &tam);
+    pares = numerosPares(&v, tam);
+    printf("Numero de pares %d\n", pares);
 }
