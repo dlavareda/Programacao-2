ebe22e2c369b34196eadfea33bb75f5839a9b8cc
Diogo Lavareda <diogolavareda9@hotmail.com>
2019-05-14 14:13:26 +0100

    update

--- Folha 8.2 - Folha Ordenacao/10.c
+++ Folha 8.2 - Folha Ordenacao/10.c
@@ -8,8 +8,13 @@ typedef struct{
 
 
 int main(){
-    NOTAS * v;
-    int tamV;
-    v = LerVetoresNotas("/home/dlavareda/dados3.txt", &tamV);
+    NOTAS * v, *NEG, *POS;
+    int tamV, tamNeg, tamPos;
+    v = LerVetoresNotas("/home/dlavareda/Documents/UBI/Programação 2/programacao2/Folha 8.2 - Folha Ordenacao/dados3.txt", &tamV);
     MostrarVectorNotas(v,tamV);
+    OrdenarQuickSort_NOTAS(v, 0, tamV-1);
+    MostrarVectorNotas(v,tamV);
+    ConstruirVectorNOtas(v, tamV, &NEG, &tamNeg, &POS, &tamPos);
+    MostrarVectorNotas(NEG, tamNeg);
+    MostrarVectorNotas(POS, tamPos);
 }
--- Folha 8.2 - Folha Ordenacao/ordenacao10c.h
+++ Folha 8.2 - Folha Ordenacao/ordenacao10c.h
@@ -1,23 +1,84 @@
-NOTAS *LerVetoresNotas(char *nomeFich, int *n){
+NOTAS *LerVetoresNotas(char *nomeFich, int *n)
+{
     NOTAS *x, aux;
     FILE *f;
     f = fopen(nomeFich, "r");
     *n = 0;
-    x = (NOTAS *) malloc(sizeof(NOTAS));
-    while(fscanf(f, "%d %d", &(aux.numero), &(aux.notafinal)==2)){
+    x = (NOTAS *)malloc(sizeof(NOTAS));
+    while (fscanf(f, "%d %d", &(aux.numero), &(aux.notafinal)) == 2)
+    {
         *n = *n + 1;
-        x = (NOTAS *)realloc(x, (*n)*sizeof(NOTAS));
-        x[*n-1] = aux;
+        x = (NOTAS *)realloc(x, (*n) * sizeof(NOTAS));
+        x[*n - 1] = aux;
     }
     fclose(f);
     return x;
 }
-
-void MostrarVectorNotas(NOTAS *x, int n){
+void trocar_notas(NOTAS *A, NOTAS *B)
+{
+    NOTAS Aux;
+    Aux = *A;
+    *A = *B;
+    *B = Aux;
+}
+void MostrarVectorNotas(NOTAS *x, int n)
+{
     int i;
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-       printf("%d -> %d\n", x[i].numero, x[i].notafinal);
+        printf("%d -> %d\n", x[i].numero, x[i].notafinal);
     }
     printf("\n");
+}
+int DeterminarPosicaoPivot(NOTAS *x, int inicio, int fim)
+{
+    int i, k = inicio;
+    for (i = inicio+1; i <= fim; i++)
+    {
+        if (x[i].notafinal < x[inicio].notafinal)
+        {
+            k++;
+            trocar_notas(&(x[i]), &(x[k]));
+        }
+    }
+    trocar_notas(&(x[inicio]), &(x[k]));
+
+    return k;
+}
+void OrdenarQuickSort_NOTAS(NOTAS *x, int inicio, int fim)
+{
+    int k;
+    if (inicio < fim)
+    {
+        {
+            k = DeterminarPosicaoPivot(x, inicio, fim);
+            OrdenarQuickSort_NOTAS(x, inicio, k - 1);
+            OrdenarQuickSort_NOTAS(x, k + 1, fim);
+        }
+    }
+}
+
+void ConstruirVectorNOtas(NOTAS *v, int n, NOTAS **NEG, int *NN, NOTAS **POS, int *NP){
+    int i,k;
+    OrdenarQuickSort_NOTAS(v, 0, n-1);
+    k = 0;
+    while (k<n && v[k].notafinal<10)
+    {
+        k++;
+    }
+    *NN = k;
+    *NP = n-k;
+    *NEG = (NOTAS *)malloc((*NN)*sizeof(NOTAS));
+    *POS = (NOTAS *)malloc((*NP)*sizeof(NOTAS));
+    for (i = 0; i < k; i++)
+    {
+        (*NEG)[i] = v[i];
+    }
+    for (i = k; i < n; i++)
+    {
+        (*POS)[i-k]=v[i];
+    }
+    
+    
+    
 }
